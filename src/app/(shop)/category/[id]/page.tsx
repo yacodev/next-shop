070c8b7e2bb80dcc69b9820b27c4197b7749a733
yacodev <cyaco33@gmail.com>
2024-11-12 15:06:00 -0500

    finish layout

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,21 +1,40 @@
-import { notFound } from 'next/navigation';
+import { initialData } from '@/seed/seed';
+//import { notFound } from 'next/navigation';
+import { ProductGrid, Title } from '@/components';
+import { Category } from '@/interfaces';
 
+const seedProducts = initialData.products;
 interface CategoryPageProps {
   params: {
-    id: string;
+    id: Category;
   };
 }
 
 export default function CategoryPage({ params }: CategoryPageProps) {
   const { id } = params;
 
-  if (id === 'kids') {
+  const products = seedProducts.filter((product) => product.gender === id);
+  const labels: Record<Category, string> = {
+    men: 'para Hombres',
+    women: 'para Mujeres',
+    kid: 'para Niños',
+    unisex: 'Unisex',
+  };
+
+  /* if (id === 'kids') {
     notFound();
-  }
+  } */
 
   return (
     <div>
       <h1>Category Page {id}</h1>
+      <Title
+        title={`Articulos de ${labels[id]}`}
+        subtitle='Todos los productos'
+        className='mb-2'
+      />
+
+      <ProductGrid products={products} />
     </div>
   );
 }
--- src/app/auth/new-account/page.tsx
+++ src/app/auth/new-account/page.tsx
@@ -1,8 +1,41 @@
+import Link from 'next/link';
+
 export default function NewAccountPage() {
   return (
-    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-      <div className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
-        <h1> new- account - page</h1>
+    <div className='flex flex-col min-h-screen pt-32 sm:pt-52'>
+      <h1 className={'text-4xl mb-5'}>Nueva cuenta</h1>
+
+      <div className='flex flex-col'>
+        <label htmlFor='email'>Nombre completo</label>
+        <input
+          className='px-5 py-2 border bg-gray-200 rounded mb-5'
+          type='text'
+        />
+
+        <label htmlFor='email'>Correo electrónico</label>
+        <input
+          className='px-5 py-2 border bg-gray-200 rounded mb-5'
+          type='email'
+        />
+
+        <label htmlFor='email'>Contraseña</label>
+        <input
+          className='px-5 py-2 border bg-gray-200 rounded mb-5'
+          type='email'
+        />
+
+        <button className='btn-primary'>Crear cuenta</button>
+
+        {/* divisor l ine */}
+        <div className='flex items-center my-5'>
+          <div className='flex-1 border-t border-gray-500'></div>
+          <div className='px-2 text-gray-800'>O</div>
+          <div className='flex-1 border-t border-gray-500'></div>
+        </div>
+
+        <Link href='/auth/login' className='btn-secondary text-center'>
+          Ingresar
+        </Link>
       </div>
     </div>
   );
