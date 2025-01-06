30fcf72626c1676395853da1494d3213a570d5e0
yacodev <cyaco33@gmail.com>
2025-01-06 17:52:54 -0500

    fix typos

--- src/actions/product/delete-product-image.ts
+++ src/actions/product/delete-product-image.ts
@@ -1,18 +1,15 @@
 'use server';
 
-import { revalidatePath } from 'next/cache';
 import prisma from '@/lib/prisma';
 import { v2 as cloudinary } from 'cloudinary';
-
+import { revalidatePath } from 'next/cache';
 cloudinary.config(process.env.CLOUDINARY_URL ?? '');
 
 export const deleteProductImage = async (imageId: number, imageUrl: string) => {
-  console.log('server-actions', imageId, imageUrl);
-
-  if (imageUrl.startsWith('http')) {
+  if (!imageUrl.startsWith('http')) {
     return {
       ok: false,
-      error: 'No se puede eliminar la imagen',
+      error: 'No se pueden borrar imagenes de FS',
     };
   }
 
@@ -33,14 +30,15 @@ export const deleteProductImage = async (imageId: number, imageUrl: string) => {
       },
     });
 
+    // Revalidar los paths
     revalidatePath(`/admin/products`);
     revalidatePath(`/admin/product/${deletedImage.product.slug}`);
     revalidatePath(`/product/${deletedImage.product.slug}`);
   } catch (error) {
-    console.error('Error deleting image', error);
+    console.log(error);
     return {
       ok: false,
-      error: 'Error al eliminar la imagen',
+      message: 'No se pudo eliminar la imagen',
     };
   }
 };
